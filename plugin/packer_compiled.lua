4dbb97c12fcdc8b243caed785069477418752643
hereliesadvaith <hereliesadvaith@gmail.com>
2023-06-02 06:59:25 +0530

    lspconfig

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -174,11 +174,6 @@ _G.packer_plugins = {
     path = "/home/hereliesadvaith/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
-  ["typescript.nvim"] = {
-    loaded = true,
-    path = "/home/hereliesadvaith/.local/share/nvim/site/pack/packer/start/typescript.nvim",
-    url = "https://github.com/jose-elias-alvarez/typescript.nvim"
-  },
   ["vim-maximizer"] = {
     loaded = true,
     path = "/home/hereliesadvaith/.local/share/nvim/site/pack/packer/start/vim-maximizer",
